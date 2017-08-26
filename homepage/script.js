671fcdd3d52d1c96dcd478980048ab15043d70bb
Wang Jie <doudou19758@gmail.com>
2017-08-27 01:01:11 +0800

    make homepage could change background image

--- homepage/script.js
+++ homepage/script.js
@@ -1,53 +1,31 @@
-var i18nmsg = {
-  msg0: {
-    cn: '我是王孑，欢迎访问我的个人主页。你可以在下面这些地方找到我',
-    en: 'I\'m Wang Jie. I\'m a student living in China Mainland. I\'m interested in web development, algorithm and gaming. Here is my home page. You can learn me more by following links.'
-  },
-  msg1: {
-    cn: '你可以通过以下方式联系我',
-    en: 'If you want to contact me, you can try following ways.'
-  }
-}
-
-function changelang(lang) {
-  for (let id in i18nmsg) {
-    document.getElementById(id).innerHTML = i18nmsg[id][lang]
-  }
-}
-
-window.onhashchange = function() {
-  changelang(document.location.hash.substr(1))
-}
-
 var mail = document.getElementsByClassName('code')[0]
-mail.onmouseover = function(e) {
-  mail.innerHTML = atob(mail.innerHTML)
-  mail.onmouseover = undefined
-}
+mail.innerHTML = atob('aUBpOGUubmV0')
 
-function bg() {
-  if (window.innerHeight < window.innerWidth) {
-    fetchimg('https://unsplash.it/1080/600/?random')
-  } else {
-    fetchimg('https://unsplash.it/600/1080/?random')
-  }
-}
+function fetchimg() {
+  const url = window.innerHeight < window.innerWidth ?
+    'https://unsplash.it/1080/600/?random' :
+    'https://unsplash.it/600/1080/?random'
 
-bg()
-
-function yy() {
-  fetch('https://api.lwl12.com/hitokoto/main/get').then(data => data.text()).then(data => {
-    document.getElementById('yy').innerHTML = data
-  })
-}
-
-yy()
-
-function fetchimg(url) {
-  fetch(url)
+  return fetch(url)
     .then(d => d.blob())
     .then(d => URL.createObjectURL(d))
+}
+
+function changebg() {
+  const bg = document.getElementById('bg')
+  const newItem = document.createElement('div')
+  fetchimg()
     .then(d => {
-      document.getElementsByClassName('container')[0].style.backgroundImage = `url(${d})`
+      newItem.style.backgroundImage = `url(${d})`
+      newItem.setAttribute('class', 'cur')
+      bg.appendChild(newItem)
+      setTimeout(() => {
+        newItem.style.opacity = '1'
+        setTimeout(() => {
+          changebg()
+        }, 6000)
+      }, 1000)
     })
 }
+
+changebg()
